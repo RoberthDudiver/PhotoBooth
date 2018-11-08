742e747f72f80ccb75314785497e08aa97e91504
Roberth Duduver <roberth@n5now.com>
2018-11-07 21:41:10 -0300

    Fix: All files

---
+++ Twitterpost/Twitterpost/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Twitterpost
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private async  void Form1_Load(object sender, EventArgs e)
+        {
+            //var twitter = new TwitterApi(ConsumerKey, ConsumerKeySecret, AccessToken, AccessTokenSecret);
+            var twitter = new TwitterApi("IEgzSo0YDyybAWf4Ec2s2R5PY", "7VMUS6xqv5SEF0uCk1qNqoJabO7JI6mRbvfV6FQb0WOYf1bI3b", "121770924-1yid1npyGLAYB4ujlVJQD1KpE1Id8MMIY3JoTtpG", "XHNZM8VlQNQpx5By0iAha9PgekEkWwsJifPyg7gt5Gbof");
+            Task<string> task = Task.Run(() => twitter.Tweet("Estoy Orgulloso de mi Mejor Amigo @Roberthdum, El mejor programador del mundo"));         
+             var response1 = await task;
+             var response = response1;
+             Console.WriteLine(response);
+             MessageBox.Show(response);
+        }
+    }
+}
---
+++ Twitterpost/Twitterpost/TwitterApi.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Net;
+using System.Net.Http;
+
+namespace Twitterpost
+{
+  
+    /// </summary>
+    class TwitterApi
+    {
+        const string TwitterApiBaseUrl = "https://api.twitter.com/1.1/";
+        readonly string consumerKey, consumerKeySecret, accessToken, accessTokenSecret;
+        readonly HMACSHA1 sigHasher;
+        readonly DateTime epochUtc = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        
+        public TwitterApi(string consumerKey, string consumerKeySecret, string accessToken, string accessTokenSecret)
+        {
+            this.consumerKey = consumerKey;
+            this.consumerKeySecret = consumerKeySecret;
+            this.accessToken = accessToken;
+            this.accessTokenSecret = accessTokenSecret;
+
+            sigHasher = new HMACSHA1(new ASCIIEncoding().GetBytes(string.Format("{0}&{1}", consumerKeySecret, accessTokenSecret)));
+        }
+
+        public Task<string> Tweet(string text)
+        {
+            var data = new Dictionary<string, string> {
+			{ "status", text },
+			{ "trim_user", "1" }
+		};
+
+            return SendRequest("statuses/update.json", data);
+        }
+
+        Task<string> SendRequest(string url, Dictionary<string, string> data)
+        {
+            var fullUrl = TwitterApiBaseUrl + url;
+
+            // Timestamps are in seconds since 1/1/1970.
+            var timestamp = (int)((DateTime.UtcNow - epochUtc).TotalSeconds);
+
+            // Add all the OAuth headers we'll need to use when constructing the hash.
+            data.Add("oauth_consumer_key", consumerKey);
+            data.Add("oauth_signature_method", "HMAC-SHA1");
+            data.Add("oauth_timestamp", timestamp.ToString());
+            data.Add("oauth_nonce", "a"); // Required, but Twitter doesn't appear to use it, so "a" will do.
+            data.Add("oauth_token", accessToken);
+            data.Add("oauth_version", "1.0");
+
+            // Generate the OAuth signature and add it to our payload.
+            data.Add("oauth_signature", GenerateSignature(fullUrl, data));
+
+            // Build the OAuth HTTP Header from the data.
+            string oAuthHeader = GenerateOAuthHeader(data);
+
+            // Build the form data (exclude OAuth stuff that's already in the header).
+            var formData = new FormUrlEncodedContent(data.Where(kvp => !kvp.Key.StartsWith("oauth_")));
+
+            return SendRequest(fullUrl, oAuthHeader, formData);
+        }
+
+        /// <summary>
+        /// Generate an OAuth signature from OAuth header values.
+        /// </summary>
+        string GenerateSignature(string url, Dictionary<string, string> data)
+        {
+            var sigString = string.Join(
+                "&",
+                data
+                    .Union(data)
+                    .Select(kvp => string.Format("{0}={1}", Uri.EscapeDataString(kvp.Key), Uri.EscapeDataString(kvp.Value)))
+                    .OrderBy(s => s)
+            );
+
+            var fullSigData = string.Format(
+                "{0}&{1}&{2}",
+                "POST",
+                Uri.EscapeDataString(url),
+                Uri.EscapeDataString(sigString.ToString())
+            );
+
+            return Convert.ToBase64String(sigHasher.ComputeHash(new ASCIIEncoding().GetBytes(fullSigData.ToString())));
+        }
+
+      
+        string GenerateOAuthHeader(Dictionary<string, string> data)
+        {
+            return "OAuth " + string.Join(
+                ", ",
+                data
+                    .Where(kvp => kvp.Key.StartsWith("oauth_"))
+                    .Select(kvp => string.Format("{0}=\"{1}\"", Uri.EscapeDataString(kvp.Key), Uri.EscapeDataString(kvp.Value)))
+                    .OrderBy(s => s)
+            );
+        }
+
+    
+        async Task<string> SendRequest(string fullUrl, string oAuthHeader, FormUrlEncodedContent formData)
+        {
+            using (var http = new HttpClient())
+            {
+                http.DefaultRequestHeaders.Add("Authorization", oAuthHeader);
+
+                var httpResp = await http.PostAsync(fullUrl, formData);
+                var respBody = await httpResp.Content.ReadAsStringAsync();
+
+                return respBody;
+            }
+        }
+    }
+}
